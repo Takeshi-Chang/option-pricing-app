bb12393d4fcb02884419a9f3ffcce3a688d4e6cb
Takeshi Chang <takeshichang2028@u.northwestern.edu>
2026-04-20 09:45:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,15 @@ import streamlit as st
 
 st.set_page_config(page_title="Option Pricing App", page_icon="📈")
 
-st.title("Tak's Simple Option Pricer")
+home = st.Page("pages/1_Home.py", title="Home", icon="🏠")
+black_scholes = st.Page("pages/2_Black_Scholes_Model.py", title="Black-Scholes Model", icon="📈")
+binomial = st.Page("pages/3_Binomial_Model.py", title="Binomial Model", icon="🌲")
+visualizations = st.Page("pages/4_Visualizations.py", title="Visualizations", icon="📊")
+about = st.Page("pages/5_About_Me.py", title="About Me", icon="🙋")
 
-st.write("""
-This web app demonstrates two fundamental option pricing models:
+pg = st.navigation(
+    [home, black_scholes, binomial, visualizations, about],
+    position="sidebar"
+)
 
-- The **Black–Scholes** Model
-  - Calculates option prices using a formula based on stock price, time, volatility, and interest rates.
-  - Best for European options that can only be exercised at expiration.
-
-- The **Binomial Tree** Model
-  - Estimates option prices by simulating step-by-step stock price movements in a tree.
-  - Flexible for European and American options that can be exercised early.
-
-Use the sidebar to navigate between pages. Enjoy!
-""")
+pg.run()
